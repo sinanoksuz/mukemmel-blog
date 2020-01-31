6150e7febbacf01acc2a04af5c124aec80bfc538
sinanoksuz <180101005@ogr.atu.edu.tr>
2020-01-31 20:20:24 +0300

    v2.2

--- pages/[postId].js
+++ pages/[postId].js
@@ -10,7 +10,6 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import MailIcon from '@material-ui/icons/Mail';
-import  Commentpage  from "./Comment";
 import { TextField } from "@material-ui/core";
 import Router from 'next/router';
 const BlogPost = ({ post,comment }) => (
